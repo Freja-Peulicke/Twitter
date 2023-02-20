573fb1c0ed726e9828e94e3e45f5eafb2225c571
frej1187 <frej1187@stud.kea.dk>
2023-02-20 13:41:19 +0100

    daily update

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
+
+from bottle import default_app, get, post, run
 from bottle import get, run, template, static_file, response, request
 import sqlite3
 
 db = sqlite3.connect('twitter.db')
+
+
+# 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
+# https://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github.com/frej1187/twitter.git
+#########################
+
+
+@post('/secret_url_for_git_hook')
+def git_update():
+    repo = git.Repo('./twitter')
+    origin = repo.remotes.origin
+    repo.create_head('main', origin.refs.main).set_tracking_branch(
+        origin.refs.main).checkout()
+    origin.pull()
+    return ""
+
+
+##############################
+# Run in AWS
+try:
+    import production
+    print("Server running on AWS")
+    application = default_app()
+# Run in local computer
+except Exception as ex:
+    print("Server running locally")
+    run(host="127.0.0.1", port=3001, debug=True, reloader=True)
+
+##############################
+
+
+@get("/")
+def _():
+    return "One"
 
 
 @get("/")

792abfbf8b15e2557032bfb5270a36d70899a439
frej1187 <frej1187@stud.kea.dk>
2023-03-02 09:47:51 +0100

    update

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,14 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  mode:"jit",
+  mode: "jit",
   content: ["../views/*.html"],
   theme: {
     extend: {
       colors: {
-        twitter: "#1d9bf0",
-        grey: "#0f1419"
+        'twitter': '#1d9bf0',
+        'twitter-dark': '#1a8cd8',
+        'dim': '#15202b',
+        'dim-border': '#38444d'
+      },
+      width: {
+        '4.5': '1.125rem'
       }
     },
+    screens: {
+      'xs': '500px',
+      ...defaultTheme.screens,
+    }
   },
   plugins: [],
 }

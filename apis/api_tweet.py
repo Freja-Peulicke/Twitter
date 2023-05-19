35386e2bb39977a5f168277921ceb1360ce17427
frej1187 <frej1187@stud.kea.dk>
2023-05-19 23:57:36 +0200

    trying to make image upload work on pythonanywhere

--- apis/api_tweet.py
+++ apis/api_tweet.py
@@ -16,44 +16,49 @@ def _():
 
         the_image = request.files.get("img-upload")
         if the_image is not None:
+            print("### 1")
             image_content = the_image.file.read()
+            print("### 2")
             the_image.file.seek(0)
+            print("### 3")
             mime_type = magic.from_buffer(image_content,mime=True)
+            print("### 4")
             if mime_type not in ("image/png", "image/jpg", "image/jpeg"):
+                print("### 4a")
                 raise Exception("Please, try again. Image not allowed.")
             # name, ext = os.path.splitext(the_image.filename)
+            print("### 5")
             ext = pathlib.Path(the_image.filename).suffix
+            print("### 6")
             if ext not in ( ".png", ".jpg", ".jpeg"):
+                print("### 6a")
                 raise Exception("Please, try again. Image not allowed.")
+            
+            print("### 7")
             image_name = str(uuid.uuid4().hex)
+            print("### 8")
             image_name = image_name + ext
+            print("### 9")
             #print("############")
             #print(f"{x.base_dir}images/tweets/{image_name}")
             the_image.save(f"{x.base_dir}images/tweets/{image_name}")
             #the_image.save(f"images/tweets/{image_name}")
+            print("### 10")
         else:
             image_name = ""
         
         db = x.db()
-        print("### 1")
         tweet_id = str(uuid.uuid4().hex)
-        print("### 2")
         tweet_message = request.forms.get("message")
-        print("### 3")
         tweet_image = image_name
-        print("### 4")
         tweet_created_at = int(time.time())
-        print("### 5")
         tweet_user_fk = logged_in_user['user_id']
-        print("### 6")
         db.execute("INSERT INTO tweets (tweet_id, tweet_message, tweet_image, tweet_created_at, tweet_user_fk) VALUES(?, ?, ?, ?, ?)",
                    (tweet_id, tweet_message, tweet_image, tweet_created_at, tweet_user_fk))
-        print("### 7")
         db.commit()
-        print("### 8")
         return {"info": "ok", "tweet_id": tweet_id}
     except Exception as ex:  # SOMETHING IS WRONG
-        print("### 9")
+        print("### Error")
         response.status = 400
         return {"info": str(ex)}
     finally:  # This will always take place
